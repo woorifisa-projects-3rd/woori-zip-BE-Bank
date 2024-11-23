562fd6430359a25b39659b1609c9b3635b7fbb5d
rlfrkdms1 <rlfrkdms1@naver.com>
2024-11-23 19:09:08 +0900

    [feat/#6]: interceptor, argumentResolver 추가

--- src/main/java/fisa/woorizip/bank/common/config/AuthConfig.java
+++ src/main/java/fisa/woorizip/bank/common/config/AuthConfig.java
@@ -1,15 +1,41 @@
 package fisa.woorizip.bank.common.config;
 
+import fisa.woorizip.bank.auth.AuthArgumentResolver;
+import fisa.woorizip.bank.auth.AuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class AuthConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+    private final AuthArgumentResolver authArgumentResolver;
+
+    @Autowired
+    public AuthConfig(AuthInterceptor authInterceptor, AuthArgumentResolver authArgumentResolver) {
+        this.authInterceptor = authInterceptor;
+        this.authArgumentResolver = authArgumentResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor);
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
