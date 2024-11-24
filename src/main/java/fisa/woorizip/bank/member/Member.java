408ca8c0261aa553a21a6da81a1a7dec37333d9c
rlfrkdms1 <rlfrkdms1@naver.com>
2024-11-24 23:05:02 +0900

    [feat/#6]: member 엔티티의 필드 추가

--- src/main/java/fisa/woorizip/bank/member/Member.java
+++ src/main/java/fisa/woorizip/bank/member/Member.java
@@ -1,15 +1,23 @@
 package fisa.woorizip.bank.member;
 
+import fisa.woorizip.bank.spendinghistory.SpendingHistory;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -28,5 +36,45 @@ public class Member {
     @Column(name = "password", nullable = false)
     private String password;
 
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "customer_id", nullable = false)
+    private String customerId;
+
+    @Column(name = "birthday", nullable = false)
+    private LocalDate birthday;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "sex", nullable = false)
+    private Sex sex;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "member_rank", nullable = false)
+    private Rank rank;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "life_stage", nullable = false)
+    private LifeStage lifeStage;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "earnings_type")
+    private EarningsType earningsType;
+
+    @Column(name = "earnings_fee")
+    private Long earningsFee;
+
+    @Column(name = "credit_score")
+    private Integer creditScore;
+
+    @Column(name = "debt", nullable = false, columnDefinition = "BIGINT DEFAULT 0")
+    private long debt;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "spending_history_id", nullable = false)
+    private SpendingHistory spendingHistory;
 
 }
