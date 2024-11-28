a45875098b4df5c331ded0ff0a0f01001c1c6b01
rlfrkdms1 <rlfrkdms1@naver.com>
2024-11-28 11:18:28 +0900

    [feat/#6]: 회원 정보 반환 response 추가

---
+++ src/main/java/fisa/woorizip/bank/member/dto/ShowMemberDataResponse.java
@@ -0,0 +1,38 @@
+package fisa.woorizip.bank.member.dto;
+
+import fisa.woorizip.bank.member.LifeStage;
+import fisa.woorizip.bank.member.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class ShowMemberDataResponse {
+
+    private final String customerId;
+    private final String name;
+    private final int gender;
+    private final LocalDate birthday;
+    private final Integer creditScore;
+    private final int membership;
+    private final LifeStage lifeStage;
+    private final long availableAssets;
+    private final SpendingHistoryResponse spendingHistory;
+
+    public static ShowMemberDataResponse from(Member member) {
+        return ShowMemberDataResponse.builder()
+                .customerId(member.getCustomerId())
+                .name(member.getName())
+                .gender(member.getGender())
+                .birthday(member.getBirthday())
+                .membership(member.getMemberShip())
+                .lifeStage(member.getLifeStage())
+                .availableAssets(member.getAvailableAssets())
+                .spendingHistory(SpendingHistoryResponse.from(member.getSpendingHistory()))
+                .build();
+    }
+}
